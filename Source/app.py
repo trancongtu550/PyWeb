59ff9fae2202ab130b6c1c73328f2994ce1077b5
trancongtu550 <trancongtu550@gmail.com>
2021-11-11 18:27:44 +0000

    complete

--- Source/app.py
+++ Source/app.py
@@ -1,44 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
-import mysql.connector
-from mysql.connector import Error
 
 app = Flask(__name__)
 
 # Change this to your secret key (can be anything, it's for extra protection)
-app.secret_key = ''
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
-app.config['MYSQL_PORT'] = 3307
+app.secret_key = '1a2b3c4d5e'
 
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = '550studios.com'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'anhem550'
+app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_PORT'] = 3307
 # Intialize MySQL
 mysql = MySQL(app)
-
-
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template("index.html")
 
 
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
-    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
         # Fetch one record and return result
         account = cursor.fetchone()
-                # If account exists in accounts table in out database
+        # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
@@ -49,17 +43,9 @@ def login():
         else:
             # Account doesnt exist or username/password incorrect
             flash("Incorrect username/password!", "danger")
-    return render_template('index.html', msg=msg)
+    return render_template('index.html', title="Login")
 
-
-@app.route('/pythonlogin/logout')
-def logout():
-    # Remove session data, this will log the user out
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    # Redirect to login page
-    return redirect(url_for('login'))
+# This will be the registration page, we need to use both GET and POST requests
 
 
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
@@ -97,6 +83,7 @@ def register():
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
+# This will be the home page, only accessible for loggedin users
 
 
 @app.route('/pythonlogin/home')
@@ -123,6 +110,14 @@ def profile():
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
+@app.route('/pythonlogin/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=5009)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5009)
