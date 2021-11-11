17c2d6ce37c1a8075afb0b5c9b166af06a443d62
trancongtu550 <trancongtu550@gmail.com>
2021-11-11 17:53:30 +0000

    bug, can't login, but still have regist

--- Source/app.py
+++ Source/app.py
@@ -29,18 +29,16 @@ def index():
 def login():
     # Output message if something goes wrong...
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            'SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
         # Fetch one record and return result
         account = cursor.fetchone()
-        # If account exists in accounts table in out database
+                # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
@@ -50,8 +48,7 @@ def login():
             return redirect(url_for('home'))
         else:
             # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
+            flash("Incorrect username/password!", "danger")
     return render_template('index.html', msg=msg)
 
 
@@ -127,6 +124,5 @@ def profile():
     return redirect(url_for('login'))
 
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5009)
+    app.run(debug=True,host='0.0.0.0', port=5009)
